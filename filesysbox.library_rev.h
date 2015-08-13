ed7692be0814bc7c6e81224cfe7a5a09ceb1b8a5
Fredrik Wikstrom <fredrik@a500.org>
2015-08-13 16:49:20 +0300

    filesysbox.library 53.3 (13.8.2015)
    - CHECKWRITABLE() now also checks if the FBXVF_READ_ONLY flag is set.
    
    - Removed code for opening and closing of intuition.library since this library
      was not used anywhere.

--- filesysbox.library_rev.h
+++ filesysbox.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	2
-#define DATE		"6.8.2015"
-#define VERS		"filesysbox.library 53.2"
-#define VSTRING		"filesysbox.library 53.2 (6.8.2015)\r\n"
-#define VERSTAG		"\0$VER: filesysbox.library 53.2 (6.8.2015)"
+#define REVISION	3
+#define DATE		"13.8.2015"
+#define VERS		"filesysbox.library 53.3"
+#define VSTRING		"filesysbox.library 53.3 (13.8.2015)\r\n"
+#define VERSTAG		"\0$VER: filesysbox.library 53.3 (13.8.2015)"
