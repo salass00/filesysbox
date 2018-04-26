b4f899a25dc8cc0e040f712519868202e9455f3f
Fredrik Wikstrom <fredrik@a500.org>
2018-04-26 11:20:49 +0300

    Bumped version to 53.7.

--- filesysbox.library_rev.h
+++ filesysbox.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	6
-#define DATE		"26.12.2015"
-#define VERS		"filesysbox.library 53.6"
-#define VSTRING		"filesysbox.library 53.6 (26.12.2015)\r\n"
-#define VERSTAG		"\0$VER: filesysbox.library 53.6 (26.12.2015)"
+#define REVISION	7
+#define DATE		"26.4.2018"
+#define VERS		"filesysbox.library 53.7"
+#define VSTRING		"filesysbox.library 53.7 (26.4.2018)\r\n"
+#define VERSTAG		"\0$VER: filesysbox.library 53.7 (26.4.2018)"
