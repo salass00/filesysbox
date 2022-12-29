8aa366bbe7da5e907130f4b3faf8cf04eda64c0f
BSzili <col.lawrence@gmail.com>
2022-12-29 11:08:21 +0100

    Generate an old-style FD file and pragma file

--- mkheaders.sh
+++ mkheaders.sh
@@ -4,6 +4,8 @@
 #
 
 sfdc --mode=clib -o include/clib/filesysbox_protos.h filesysbox_lib.sfd
+sfdc --mode=fd -o include/fd/filesysbox_lib.fd filesysbox_lib.sfd
+sfdc --mode=pragmas -o include/pragmas/filesysbox_pragmas.h filesysbox_lib.sfd
 sfdc --mode=macros --target=i386-pc-aros -o include/defines/filesysbox.h filesysbox_lib.sfd
 sfdc --mode=macros --target=m68k-amigaos -o include/inline/filesysbox.h filesysbox_lib.sfd
 sfdc --mode=proto -o include/proto/filesysbox.h filesysbox_lib.sfd
