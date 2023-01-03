3129a9954dc1ee1cab6eb283abeda41e861615c2
Fredrik Wikstrom <fredrik@a500.org>
2023-01-03 11:26:26 +0200

    Removed version number from directory name.

--- mkrelease.sh
+++ mkrelease.sh
@@ -14,37 +14,29 @@ FULLVERS=`version filesysbox.library`
 NUMVERS=`echo "${FULLVERS}" | cut -d' ' -f2`
 
 rm -rf ${DESTDIR}
-mkdir -p ${DESTDIR}/filesysbox-${NUMVERS}/Libs
-mkdir -p ${DESTDIR}/filesysbox-${NUMVERS}/Developer/AutoDocs
-mkdir -p ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/clib
-mkdir -p ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/defines
-mkdir -p ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/inline
-mkdir -p ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/libraries
-mkdir -p ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/proto
-mkdir -p ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/sys
+mkdir -p ${DESTDIR}/filesysbox/Libs
+mkdir -p ${DESTDIR}/filesysbox/Developer/AutoDocs
+mkdir -p ${DESTDIR}/filesysbox/Developer/include/clib
+mkdir -p ${DESTDIR}/filesysbox/Developer/include/defines
+mkdir -p ${DESTDIR}/filesysbox/Developer/include/inline
+mkdir -p ${DESTDIR}/filesysbox/Developer/include/libraries
+mkdir -p ${DESTDIR}/filesysbox/Developer/include/proto
+mkdir -p ${DESTDIR}/filesysbox/Developer/include/sys
 
-cp -p LICENSE.APL ${DESTDIR}/filesysbox-${NUMVERS}
-cp -p releasenotes ${DESTDIR}/filesysbox-${NUMVERS}
-cp -p filesysbox.library ${DESTDIR}/filesysbox-${NUMVERS}/Libs
-cp -p filesysbox.doc ${DESTDIR}/filesysbox-${NUMVERS}/Developer/AutoDocs
-cp -p include/clib/filesysbox_protos.h ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/clib
-cp -p include/defines/filesysbox.h ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/defines
-cp -p include/inline/filesysbox.h ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/inline
-cp -p include/libraries/filesysbox.h ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/libraries
-cp -p include/proto/filesysbox.h ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/proto
-cp -p include/sys/statvfs.h ${DESTDIR}/filesysbox-${NUMVERS}/Developer/include/sys
-
-echo "Short:        A FUSE compatible file system layer" > ${DESTDIR}/filesysbox-${NUMVERS}/filesysbox.readme
-echo "Author:       Leif Salomonsson, Fredrik Wikstrom" >> ${DESTDIR}/filesysbox-${NUMVERS}/filesysbox.readme
-echo "Uploader:     Fredrik Wikstrom <fredrik@a500.org>" >> ${DESTDIR}/filesysbox-${NUMVERS}/filesysbox.readme
-echo "Type:         util/libs" >> ${DESTDIR}/filesysbox-${NUMVERS}/filesysbox.readme
-echo "Version:      ${NUMVERS}" >> ${DESTDIR}/filesysbox-${NUMVERS}/filesysbox.readme
-echo "Architecture: ${HOST}" >> ${DESTDIR}/filesysbox-${NUMVERS}/filesysbox.readme
-echo "" >> ${DESTDIR}/filesysbox-${NUMVERS}/filesysbox.readme
-cat README >> ${DESTDIR}/filesysbox-${NUMVERS}/filesysbox.readme
+cp -p README ${DESTDIR}/filesysbox
+cp -p LICENSE.APL ${DESTDIR}/filesysbox
+cp -p releasenotes ${DESTDIR}/filesysbox
+cp -p filesysbox.library ${DESTDIR}/filesysbox/Libs
+cp -p filesysbox.doc ${DESTDIR}/filesysbox/Developer/AutoDocs
+cp -p include/clib/filesysbox_protos.h ${DESTDIR}/filesysbox/Developer/include/clib
+cp -p include/defines/filesysbox.h ${DESTDIR}/filesysbox/Developer/include/defines
+cp -p include/inline/filesysbox.h ${DESTDIR}/filesysbox/Developer/include/inline
+cp -p include/libraries/filesysbox.h ${DESTDIR}/filesysbox/Developer/include/libraries
+cp -p include/proto/filesysbox.h ${DESTDIR}/filesysbox/Developer/include/proto
+cp -p include/sys/statvfs.h ${DESTDIR}/filesysbox/Developer/include/sys
 
 rm -f filesysbox.${HOST}.7z
-7za u filesysbox.${HOST}.7z ./${DESTDIR}/filesysbox-${NUMVERS}
+7za u filesysbox.${HOST}.7z ./${DESTDIR}/filesysbox
 
 rm -rf ${DESTDIR}
 
