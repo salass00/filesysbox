5000c14c0d83efa9690ffc06f66ce7a52f90a8f4
Fredrik Wikstrom <fredrik@a500.org>
2023-07-21 19:30:55 +0300

    Bumped version to 54.5.

--- filesysbox.library_rev.h
+++ filesysbox.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		54
-#define REVISION	4
-#define DATE		"23.6.2023"
-#define VERS		"filesysbox.library 54.4"
-#define VSTRING		"filesysbox.library 54.4 (23.6.2023)\r\n"
-#define VERSTAG		"\0$VER: filesysbox.library 54.4 (23.6.2023)"
+#define REVISION	5
+#define DATE		"21.7.2023"
+#define VERS		"filesysbox.library 54.5"
+#define VSTRING		"filesysbox.library 54.5 (21.7.2023)\r\n"
+#define VERSTAG		"\0$VER: filesysbox.library 54.5 (21.7.2023)"
