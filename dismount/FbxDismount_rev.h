7c6d62bee07b880330ff1ea843b256459dfe89ee
Fredrik Wikstrom <fredrik@a500.org>
2026-02-02 10:53:15 +0200

    Bumped FbxDismount to current date.

--- dismount/FbxDismount_rev.h
+++ dismount/FbxDismount_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		54
 #define REVISION	1
-#define DATE		"28.6.2023"
+#define DATE		"2.2.2026"
 #define VERS		"FbxDismount 54.1"
-#define VSTRING		"FbxDismount 54.1 (28.6.2023)\r\n"
-#define VERSTAG		"\0$VER: FbxDismount 54.1 (28.6.2023)"
+#define VSTRING		"FbxDismount 54.1 (2.2.2026)\r\n"
+#define VERSTAG		"\0$VER: FbxDismount 54.1 (2.2.2026)"
