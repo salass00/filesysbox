9d913687a42dd0e5ced59664008998df63c24578
Fredrik Wikstrom <fredrik@a500.org>
2026-03-19 22:02:25 +0200

    Bumped version to 54.10.

--- filesysbox.library_rev.h
+++ filesysbox.library_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		54
-#define REVISION	9
-#define DATE		"18.2.2026"
-#define VERS		"filesysbox.library 54.9"
-#define VSTRING		"filesysbox.library 54.9 (18.2.2026)\r\n"
-#define VERSTAG		"\0$VER: filesysbox.library 54.9 (18.2.2026)"
+#define REVISION	10
+#define DATE		"19.3.2026"
+#define VERS		"filesysbox.library 54.10"
+#define VSTRING		"filesysbox.library 54.10 (19.3.2026)\r\n"
+#define VERSTAG		"\0$VER: filesysbox.library 54.10 (19.3.2026)"
